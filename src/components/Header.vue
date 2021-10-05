991a5680e81ea533fa581ccd75706b8483c1d225
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-05 16:34:28 +0200

    Aggiunta input e button statici

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        <input type="text" placeholder="Inserisci il titolo di un film">
+        <button>Cerca</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.header{
+    height: 80px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 50px;
+    
+    input{
+        padding: 10px;
+        width: 200px;
+        height: 20px;
+    }
+    button{
+        padding: 10px;
+        height: 44px;
+    }
+}
+
+</style>
