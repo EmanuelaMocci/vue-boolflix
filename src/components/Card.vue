51ecd0cc6667c2f319b00f7f7626c8047b11ffc5
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-06 15:07:53 +0200

    Aggiunta bandiere lingua

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,16 +3,21 @@
     <div class="card">
         <h3 v-if="details.title">{{details.title}}</h3>
         <h3 v-else>{{details.name}}</h3>
-
+<hr>
         <h4>{{details.original_title ? details.original_title : details.original_name}}</h4>
-        <div>{{details.original_language}}</div>
+        <div><lang-flag :iso='details.original_language'/></div>
         <div>{{details.vote_avarage}}</div>
     </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
+
 export default {
     name: "Card",
-    props: ['details']
+    props: ['details'],
+    components: {
+        LangFlag
+    }
 }
 </script>
