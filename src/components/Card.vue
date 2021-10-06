6a03b280823f52954dee4f015800c6d9b39f7fb8
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-10-06 16:17:41 +0200

    Aggiunta copertina del film/serie tv

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,8 @@
     <div class="card">
         <h3 v-if="details.title">Titolo: {{details.title}}</h3>
         <h3 v-else> Titolo: {{details.name}}</h3>
-        <h4>Titolo originale: {{details.original_title ? details.original_title : details.original_name}}</h4>
+        <h3>Titolo originale: {{details.original_title ? details.original_title : details.original_name}}</h3>
+        <img :src= "`https://image.tmdb.org/t/p/w500/${details.poster_path}`">
         <div>Lingua: <lang-flag :iso='details.original_language'/></div>
         <div>Voto: {{details.vote_average}}</div>
     </div>
